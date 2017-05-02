544582d36890d09f07c552d4b829d325e94f7006
Angad Gill <angadsinghgill@gmail.com>
2017-05-02 16:56:05 -0700

    added frontend service conf file

--- frontend/frontend_startup.sh
+++ frontend/frontend_startup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Config NGINX
-cp /home/kmeans-service/frontend/frontend_nginx_config /etc/nginx/sites-available/frontend
-ln -s /etc/nginx/sites-available/frontend /etc/nginx/sites-enabled
+sudo cp /home/ubuntu/kmeans-service/frontend/frontend_nginx_config /etc/nginx/sites-available/frontend
+sudo ln -s /etc/nginx/sites-available/frontend /etc/nginx/sites-enabled
 sudo service nginx restart
 
 # Config frontend upstart script
-cp /home/kmeans-service/frontend/frontend.conf /etc/init/frontend.conf
+sudo cp /home/ubuntu/kmeans-service/frontend/frontend.conf /etc/init/frontend.conf
 sudo service frontend start
